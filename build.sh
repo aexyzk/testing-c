5cf362336263970dd17972d85a246eb8751b4de3
aexyzk <aexyzk@proton.me>
2025-01-10 11:41:24 -0600

    raylib

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-clang main.c -o main_clang
-gcc main.c -o main_gcc
+#!/bin/bash
+
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <raylib.h>
 
 int fail(char msg[]) {
     printf("\033[91m[FAIL] %s", msg);
@@ -21,13 +22,22 @@ int ok(char msg[]) {
     return 0;
 }
 
-int main() {
-    int integer = 3;
-    double decimal = 5.3;
-    char letter = 'a';
-    char string[] = "hello world";
+int main(void) {
+    const int SCREENWIDTH = 640;
+    const int SCREENHEIGHT = 480;
 
-    fail("hello");
+    InitWindow(SCREENWIDTH,SCREENHEIGHT,"raylib");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("owo", 200, 200, 20, BLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
