7e6bdba580efea1f063d93a3229f781332bd3a89
aexyzk <aexyzk@proton.me>
2025-01-09 20:09:40 -0600

    oof

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # testing-c
-Messing around with C not really maybe this becomes something, maybe not.
+Messing around with C not really anything maybe this becomes something, maybe not.
--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int fail(char msg[]) {
+    printf("\033[91m[FAIL] %s", msg);
+    return 0;
+}
 
+int info(char msg[]) {
+    printf("[INFO] %s", msg);
+    return 0;
+}
+
+int warn(char msg[]) {
+    printf("\033[93m[WARN] %s", msg);
+    return 0;
+}
+
+int ok(char msg[]) {
+    printf("\033[96m[OK] %s", msg);
+    return 0;
+}
+
+int main() {
     int integer = 3;
     double decimal = 5.3;
     char letter = 'a';
-    char string[] = "hello world"
+    char string[] = "hello world";
+
+    fail("hello");
 
     return 0;
 }
